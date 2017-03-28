3a5132ac76441b35ae6969bd6c71a935a061b8b6
王增凯 <wangzengkai_sx@qiyi.com>
2017-03-28 16:57:12 +0800

    add comment model

--- config/default.js
+++ config/default.js
@@ -3,7 +3,7 @@ module.exports = {
   session: {
     secret: 'myblog',
     key: 'myblog',
-    maxAge: 2592000000
+    maxAge: 7*24*60*60 // 一周有效
   },
   mongodb: 'mongodb://localhost:27017/myblog'
 }
